96e9d43ac1c8099cce9c2eb10a8a9d6ce6c22ef0
AbyChan <crystal.tyan@gmail.com>
2015-12-22 23:42:33 +0800

    fetch data form process

--- emacs-feedjs.el
+++ emacs-feedjs.el
@@ -1,10 +1,18 @@
 
+(require 'json)
 
-(defvar feedjs--process "/Users/tyan/PROJECT/NodeAtom/app.js")
+(setq feedjs--process "/Users/soul/PROJECT/NodeAtom/app.js")
+
+
+
+(defvar feedjs--buffer "*FeedJs*")
+
+(defvar feedjs--process-name "FeedJs")
 
 (defun fetch-feed-by-process ()
   (interactive)
-  
+  (start-process feedjs--process-name feedjs--buffer
+                 "node" feedjs--process "--emacs")
   )
 
 (provide 'emacs-feedjs)
