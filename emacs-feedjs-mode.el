7a4a8f6fea782a0597e0b3097f3f1cf4d066da3f
AbyChan <crystal.tyan@gmail.com>
2015-12-23 23:36:53 +0800

    emacs-feedjs-mode

---
+++ emacs-feedjs-mode.el
@@ -0,0 +1,34 @@
+
+
+(defun feedjs-mode-buffer ()
+  "get *emacs-feedjs* buffer"
+  (get-buffer-create "*emacs-feedjs*"))
+
+(defvar emacs-feedjs-mode-map
+  (let ((map (make-sparse-keymap)))
+    (prog1 map
+      (suppress-keymap map)
+      (define-key map "q" 'quit-window)))
+  "Keymap for emacs-feedjs-mode.")
+
+(defun emacs-feedjs-mode ()
+  "Magor mode for listing feed."
+  (interactive)
+  (kill-all-local-variables)
+  (use-local-map emacs-feedjs-mode-map)
+  (setq major-mode 'emacs-feedjs-mode
+        mode-name "emacs-feedjs"
+        truncate-lines t
+        buffer-read-only t)
+  (buffer-disable-undo)
+  (hl-line-mode);highlight-changes-mode
+  (make-local-variable 'emacs-feedjs-entries)
+  (make-local-variable 'emacs-feedjs-filter)
+                                        ;(add-hook 'feedjs-update-hooks #'emacs-feedjs-update)
+
+  (run-hooks 'elfeed-search-mode-hooks))
+
+
+
+
+(provide 'emcas-feedjs-mode)
--- emacs-feedjs.el
+++ emacs-feedjs.el
@@ -1,8 +1,8 @@
+(require 'emcas-feedjs-mode)
 
 (require 'json)
 
-(setq feedjs--process "/Users/soul/PROJECT/NodeAtom/app.js")
-
+(defvar feedjs--process "/Users/soul/PROJECT/NodeAtom/app.js")
 
 
 (defvar feedjs--buffer "*FeedJs*")
@@ -14,5 +14,16 @@
   (start-process feedjs--process-name feedjs--buffer
                  "node" feedjs--process "--emacs")
   )
+
+;;;###autoload
+(defun feedjs ()
+  "Enter elfeed."
+  (interactive)
+  (switch-to-buffer (feedjs-mode-buffer))
+  (unless (eq major-mode 'emacs-feedjs-mode)
+    (emacs-feedjs))
+  ;TODO
+  )
+
 
 (provide 'emacs-feedjs)
